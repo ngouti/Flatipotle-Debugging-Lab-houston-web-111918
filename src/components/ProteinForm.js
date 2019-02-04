5c21c111bdb375bbbb971b73a3b8b0370442a7b2
github username <github email address>
2019-02-04 12:22:21 -0600

    Fixed some bugs

--- src/components/ProteinForm.js
+++ src/components/ProteinForm.js
@@ -12,8 +12,8 @@ const ProteinForm = () => {
               type="checkbox"
               value="Barbacoa"
               name="protein"
-              checked={ props.protein.includes('Barbacoa') }
-              onChange={ props.handleChange }
+              checked={ this.props.protein.includes('Barbacoa') }
+              onChange={ this.props.handleChange }
             />
             <img src={ require('../images/protein/barbacoa.png') } height="100px" width="100px" alt="barbacoa" />
             <label htmlFor="barbacoa">Barbacoa</label>
@@ -27,8 +27,8 @@ const ProteinForm = () => {
               type="checkbox"
               value="Steak"
               name="protein"
-              checked={ props.protein.includes('Steak') }
-              onChange={ props.handleChange }
+              checked={ this.props.protein.includes('Steak') }
+              onChange={ this.props.handleChange }
             />
             <img src={ require('../images/protein/steak.png') } height="100px" width="100px" alt="steak" />
             <label htmlFor="steak">Steak</label>
@@ -42,8 +42,8 @@ const ProteinForm = () => {
               type="checkbox"
               value="Chicken"
               name="protein"
-              checked={ props.protein.includes('Chicken') }
-              onChange={ props.handleChange }
+              checked={ this.props.protein.includes('Chicken') }
+              onChange={ this.props.handleChange }
             />
             <img src={ require('../images/protein/chicken.png') } height="100px" width="100px" alt="chicken" />
             <label htmlFor="chicken">Chicken</label>
@@ -57,8 +57,8 @@ const ProteinForm = () => {
               type="checkbox"
               value="Carnitas"
               name="protein"
-              checked={ props.protein.includes('Carnitas') }
-              onChange={ props.handleChange }
+              checked={ this.props.protein.includes('Carnitas') }
+              onChange={ this.props.handleChange }
             />
             <img src={ require('../images/protein/carnitas.png') } height="100px" width="100px" alt="carnitas" />
             <label htmlFor="carnitas">Carnitas</label>
@@ -72,8 +72,8 @@ const ProteinForm = () => {
               type="checkbox"
               value="Sofritas"
               name="protein"
-              checked={ props.protein.includes('Sofritas') }
-              onChange={ props.handleChange }
+              checked={ this.props.protein.includes('Sofritas') }
+              onChange={ this.props.handleChange }
             />
             <img src={ require('../images/protein/sofritas.png') } height="100px" width="100px" alt="sofritas" />
             <label htmlFor="sofritas">Sofritas</label>
